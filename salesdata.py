415dabf18da04b396878381dc0c7631f40a35500
savitri-reddy <savitri.reddy.beeram@gmail.com>
2025-08-16 12:49:47 +0530

    Add files via upload
    14-08-2025

---
+++ salesdata.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(r'C:\Users\Hanshu\Desktop\excel data_ML\Salary_Data.csv')
+
+print('Data shape:', dataset.shape)
+
+x = dataset.iloc[: , :-1]
+y = dataset.iloc[:, -1]
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2, random_state=0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+
+y_pred = regressor.predict(x_test)
+
+
+plt.scatter(x_test,y_test, color='red')
+plt.plot(x_train, regressor.predict(x_train), color='blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+m = regressor.coef_
+
+c = regressor.intercept_
+
+(m*12)+c
+
+(m*20)+c
+
